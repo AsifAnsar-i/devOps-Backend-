6c383707924fdeec468287677fda91d45cee7161
Asif Ansari <moaisfansari1999@gmail.com>
2024-08-15 02:24:23 +0530

    another change

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -60,7 +60,7 @@ router.post(
       res.cookie("auth_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite:"strict",
+        sameSite:"lax",
         maxAge: 86400000,
       });
       return res.status(200).send({ message: "User registered OK" });
